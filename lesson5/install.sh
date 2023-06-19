3c0e9a410094af949bf13a6dc9f728f16187eb97
dlwnsgud8406@gmail.com <dlwnsgud8406@gmail.com>
2023-06-19 19:00:44 +0900

    lesson4

---
+++ lesson5/install.sh
@@ -0,0 +1,5 @@
+# hello_world를 배포하기전에는 README setup.py main.py, __init__.py작성 후
+# python3 setup sdist
+
+# 배포된 hello-world를 설치하려면
+# pip3 install dist/hello-world-0.0.1.tar.gz
---
+++ lesson5/pythonstartup.py
@@ -0,0 +1,6 @@
+import types
+import uuid
+
+helpers = types.ModuleType('helpers')
+helpers.uuid4 = uuid.uuid4()
+ 
